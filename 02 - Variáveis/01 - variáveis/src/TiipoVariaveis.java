f134de9fc3a7eb55464ae88c919728db91a43a6b
Italo Silva <italo_j.s@hotmail.com>
2024-04-30 18:34:14 -0300

    commit

--- 02 - Variáveis/01 - variáveis/src/TiipoVariaveis.java
+++ 02 - Variáveis/01 - variáveis/src/TiipoVariaveis.java
@@ -1,5 +1,20 @@
 public class TiipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 21070333;
+        long cpf = 98765432109l;
+        float pi = 3.14F;
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+       final double valorDePi = 3.14;
+
     }
 }
