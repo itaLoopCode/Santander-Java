7121846a25c43b006c12550e84ef33a28865f82e
Italo Silva <italo_j.s@hotmail.com>
2024-05-06 15:13:57 -0300

    committed

--- 08 - controle de fluxo/src/condicaoTernaria1.java
+++ 08 - controle de fluxo/src/condicaoTernaria1.java
@@ -1,18 +1,21 @@
 // Cenário 1
+
 public class condicaoTernaria1 {
-	public static void main(String[] args) {
-		int nota = 7;
-		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
-		System.out.println(resultado);
-	}
+
+    public static void main(String[] args) {
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
 
     // Cenário 2
-public class condicaoTernaria2 {
-	public static void main(String[] args) {
-		int nota = 6;
-		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
-		System.out.println(resultado);
-	}
-}
+    public class condicaoTernaria2 {
+
+        public static void main(String[] args) {
+            int nota = 6;
+            String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+            System.out.println(resultado);
+        }
+    }
 
 }
