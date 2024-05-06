ef306e374231a9e2ddeee4fb2879b4239488ff59
Italo Silva <italo_j.s@hotmail.com>
2024-05-06 02:10:04 -0300

    committed

--- 08 - controle de fluxo/src/Condicional.java
+++
@@ -1,7 +0,0 @@
-
-public class Condicional {
-
-    public static void main(String[] args) throws Exception {
-  
-    }
-}
---
+++ 08 - controle de fluxo/src/condicaoTernaria1.java
@@ -0,0 +1,18 @@
+// Cenário 1
+public class condicaoTernaria1 {
+	public static void main(String[] args) {
+		int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+	}
+
+    // Cenário 2
+public class condicaoTernaria2 {
+	public static void main(String[] args) {
+		int nota = 6;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
+	}
+}
+
+}
